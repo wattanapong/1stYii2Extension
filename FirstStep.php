0ba59575e7fe666902b6df11a2b4037878bd383d
dkttthai <dkttthai@gmail.com>
2017-01-04 19:38:52 +0700

    Fixed Static method greeting of FirstStep

--- FirstStep.php
+++ FirstStep.php
@@ -13,19 +13,39 @@ namespace wattanapong\FirstStep;
 use Yii;
 
 class FirstStep{
-
+	const author = "Wattanapong Suttapak";
     public function init()
     {
-        if ($author === null){
-			$this->author = "Wattanapong Suttapak";
+        if ($this->author === null){
+			$this->author = self::author;
 		}
     }
+	
+	public function greet($msg){
+		return ($this->getOE()==0?"Hello":"Hi").", This is greeting from ".$this->author.".";
+	}
 
-    public static function greeting($msg){
-		return (FirstStep::getOE()==0?"Hello":"Hi").", This is greeting from ".$this->author.".";
+    public static function greetStatic(){
+		return (self::getOE()==0?"Hello":"Hi").", This is greeting from ".self::author.".";
 	}
 	
-	private static function getOE(){
+	private function getOE(){
 		return rand(0,1);
 	}
+	
+	public static function instance() {
+        return new FirstStep();
+    }
+
+    public function __call($name, $arguments) {
+        if ($name === 'greeting') {
+            call_user_func(array($this, 'greet'));
+        }
+    }
+
+    public static function __callStatic($name, $arguments) {
+        if ($name === 'greeting') {
+            call_user_func(array('FirstStep', 'greetStatic'), $arguments[0]);
+        }
+    }
 }
